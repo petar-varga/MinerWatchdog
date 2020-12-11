f73fade06b71ad850182963faee78205192d104b
petarvarga128 <petarvarga128@gmail.com>
2020-12-11 19:58:08 +0100

    Modified the GPIO interface script to use DB to get proper pin

--- RaspberryPi_scripts/GPIO_Interface_script.py
+++ RaspberryPi_scripts/GPIO_Interface_script.py
@@ -1,32 +1,47 @@
 import RPi.GPIO as GPIO
 from time import sleep
+from utils import (
+    db_insert_id, db_read, db_write, db_write_executemany
+)
 
-def restart():
+
+def restart(pc_identifier):
+    response = db_read("""SELECT *  FROM `hw_info` 
+    WHERE `pc_identifier` LIKE %s""", (pc_identifier, ))
+
+    valid_response = response[0]
+    raspberry_pin = int(valid_response["raspberry_pi_pin"])
     GPIO.setmode(GPIO.BOARD)
-    GPIO.setup(11, GPIO.OUT)
+    GPIO.setup(raspberry_pin, GPIO.OUT)
 
     # On
-    print("turn on")
-    GPIO.output(11, GPIO.HIGH)
+    print(f"turn ON pin {raspberry_pin}")
+    GPIO.output(raspberry_pin, GPIO.HIGH)
     # Wait a bit
     sleep(1)
-    print("turn off")
+    print(f"turn OFF pin {raspberry_pin}")
     # Off
-    GPIO.output(11, GPIO.LOW)
+    GPIO.output(raspberry_pin, GPIO.LOW)
     GPIO.cleanup()
 
-def start():
+def start(pc_identifier):
+    response = db_read("""SELECT *  FROM `hw_info` 
+    WHERE `pc_identifier` LIKE %s""", (pc_identifier, ))
+
+    valid_response = response[0]
+    raspberry_pin = int(valid_response["raspberry_pi_pin"])
+
     GPIO.setmode(GPIO.BOARD)
-    GPIO.setup(12, GPIO.OUT)
+    GPIO.setup(raspberry_pin, GPIO.OUT)
 
     # On
-    print("turn on")
-    GPIO.output(12, GPIO.HIGH)
+    print(f"turn ON pin {raspberry_pin}")
+    GPIO.output(raspberry_pin, GPIO.HIGH)
     # Wait a bit
     sleep(1)
-    print("turn off")
+    print(f"turn OFF pin {raspberry_pin}")
     # Off
-    GPIO.output(12, GPIO.LOW)
+    GPIO.output(raspberry_pin, GPIO.LOW)
     GPIO.cleanup()
 
 if __name__ == "__main__":
